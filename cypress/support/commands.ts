286ec9ae03d39fed199d6bcec3e5a08a7a7966e8
Murat K Ozcan <34237651+muratkeremozcan@users.noreply.github.com>
2022-08-24 16:21:27 -0500

    Initial commit

---
+++ cypress/support/commands.ts
@@ -0,0 +1,13 @@
+import '@testing-library/cypress/add-commands'
+
+Cypress.Commands.add('getByCy', (selector, ...args) =>
+  cy.get(`[data-cy="${selector}"]`, ...args),
+)
+
+Cypress.Commands.add('getByCyLike', (selector, ...args) =>
+  cy.get(`[data-cy*=${selector}]`, ...args),
+)
+
+Cypress.Commands.add('getByClassLike', (selector, ...args) =>
+  cy.get(`[class*=${selector}]`, ...args),
+)
