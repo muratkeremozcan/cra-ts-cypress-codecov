7cd4e6108bcdda4dca2078023491766ed0eab433
Murat Ozcan <muratkerem@gmail.com>
2022-08-24 17:05:45 -0500

    added e2e and ct coverage

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+export default function Stepper({
+  initial = 0,
+  onChange = (num: number) => null,
+}) {
+  const [count, setCount] = useState(initial)
+
+  const increment = () => {
+    const newCount = count + 1
+    setCount(newCount)
+    onChange(newCount)
+  }
+
+  const decrement = () => {
+    const newCount = count - 1
+    setCount(newCount)
+    onChange(newCount)
+  }
+
+  return (
+    <div data-testid="stepper">
+      <button aria-label="decrement" onClick={decrement}>
+        -
+      </button>
+      {count}
+      <button aria-label="increment" onClick={increment}>
+        +
+      </button>
+    </div>
+  )
+}
